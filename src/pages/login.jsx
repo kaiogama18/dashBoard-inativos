524f0264ff85b3055ecc462432ed45a66380500c
kaioGama <kaioeduardoescar@gmail.com>
2020-03-26 20:16:37 -0400

    [Login]

---
+++ src/pages/login.jsx
@@ -0,0 +1,18 @@
+function LoginPage() {
+    return (
+        <div className="container">
+            <h1>Hello Next.js</h1>
+            <p>Let's explore different ways to style Next.js apps</p>
+            <style jsx>{`
+          .container {
+            margin: 50px;
+          }
+          p {
+            color: blue;
+          }
+        `}</style>
+        </div>
+    )
+}
+
+export default LoginPage
