f236794ff0adca3ef7547becdb159e78ffb46910
kaioGama <kaioeduardoescar@gmail.com>
2020-03-30 01:57:48 -0400

    [Ajsute] - para teste 2

--- src/components/NavBarLeft.js
+++ src/components/NavBarLeft.js
@@ -1,13 +1,8 @@
 
 const NavBarLeft = () => (
-    // <aside className="navbarleft self-center">
-    //     <img className="h-8 w-8 self-center " src="/buritech-icon.png" alt="logo"/>
-    //     {/* <i class="material-icons md-36 md-light self-center">face</i> */}
-    // </aside>
     <aside className="flex relative h-24 w-24 h-screen bg-blue-700 justify-center py-6">
         <div className="py-4">
             <img className="h-10 w-10" src="/buritech-icon.png" alt="logo" />
-
         </div>
     </aside >
 );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 
 
 const Navbar = ({ data }) => (
-  <nav className="grid grid-cols-6 gap-4 rounded-md overflow-hidden shadow bg-white h-auto p-2 px-10 ">
+  <nav className="grid grid-cols-6 gap-4 rounded-md overflow-hidden shadow bg-white h-auto p-2 px-10">
     <div className="flex col-start-1 col-end-3 items-center">
       <button class="shadow-md flex bg-blue-600 hover:bg-blue-700 rounded-full h-10 w-10 items-center justify-center">
         <i class="material-icons">add</i>
