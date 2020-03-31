569354e5f524337d40133d00b4cdfd1aafc7a515
kaioGama <kaioeduardoescar@gmail.com>
2020-03-31 17:48:57 -0400

    [Valores Matrix de Confusão] -- Add

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -40,10 +40,9 @@ const Index = props => {
         </div>
         <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4 py-4 ">
           <HorizontalBarComponents api={response_features} />
+          <Graphic />
+          <Graphic />
           <ConfusionMatrixComponents api={response_features} />
-          <Graphic />
-          <Graphic />
-          <Graphic />
           <Graphic />
         </div>
       </div>
