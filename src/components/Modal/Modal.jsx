970e72679163ff7571ab78cdd099ce6f9b2eecdd
kaioGama <kaioeduardoescar@gmail.com>
2020-04-29 02:07:34 -0400

    [tudo ok]

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    modal teste
+  </div>
+);
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,24 @@
-const Sidebar = () => (
-  <aside className="sidebar">
-    <div className="logo">
-      <img src="/buritech-icon.png" alt="logo" />
-    </div>
-  </aside>
-);
 
+
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      <div className="logo">
+        <img src="/buritech-icon.png" alt="logo" />
+      </div>
+      <div className="mt-5">
+        <button className="material-icons btn">
+          group_add
+      </button>
+      </div>
+    </aside>
+  )
+
+
+
+}
 export default Sidebar;
+
+
+
