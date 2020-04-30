f3f418553e0419cf46071c215e42aeb6c9e1cbdf
kaioGama <kaioeduardoescar@gmail.com>
2020-04-30 04:32:17 -0400

    [ok]

--- src/components/Adm/ActivateUser/ActivateUser.jsx
+++ src/components/Adm/ActivateUser/ActivateUser.jsx
@@ -15,10 +15,10 @@ export default ({ ativo, cpf }) => {
   const Activate = async (props) => {
 
     const param = (ativo == 1) ? { cpf: props.cpf, "ativo": 0 } : { cpf: props.cpf, "ativo": 1 }
-    alert("ConectApi + param: \n" + JSON.stringify(param, null, 2));
     const { menssage } = await Rota({ route, param });
-    setChecked(ativo == 1 ? true : false)
     alert(menssage)
+    return setChecked(ativo == 1 ? true : false)
+
   }
 
 
