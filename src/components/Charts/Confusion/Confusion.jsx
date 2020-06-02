b28f3a6bc838faa05a43136d31a93459d16a471c
KaioGama <kaioeduardoescar@gmail.com>
2020-06-02 15:05:45 -0400

    [ajuste] --> confusion

--- src/components/Charts/Confusion/Confusion.jsx
+++ src/components/Charts/Confusion/Confusion.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Chart from '../../Chart/Chart';
 import Rota from '../../../Routes/Rota';
-import cx from 'classnames';
 import {
   HeatMapComponent,
   Inject,
@@ -28,74 +27,56 @@ const Confusion = ({ crop }) => {
     fetchAPI();
   }, [crop])
 
+  const Plot = (data.length) ? (
+    <HeatMapComponent
+      // titleSettings={{
+      //   text: '*No eixo y: Etiqueta verdadeira *No eixo x: Etiqueta prevista',
+      // }}
+      xAxis={{
+        title: { text: 'Etiqueta prevista' },
+        valueType: 'Category',
+        labels: ['0', '1'],
+      
+      }}
+      yAxis={{
+        title: { text: 'Etiqueta verdadeira' },
+        valueType: 'Category',
+        labels: ['1', '0'],
+      }}
+      paletteSettings={{
+        palette: [
+          { color: '#C06C84' },
+          { color: '#6C5B7B' },
+          { color: '#355C7D' },
+        ],
+        type: 'Gradient',
+      }}
+      dataSource={[
+        // [data[0].true_negative, data[0].false_positive],
+        // [data[0].false_negative, data[0].true_positive],
+        [ data[0].false_positive, data[0].true_negative,],
+        [ data[0].true_positive, data[0].false_negative],
+      ]}
+      cellSettings={{
+        format: '{value}'
+    }}
+    >
+      <Inject services={[Adaptor, Legend, Tooltip]} />
+    </HeatMapComponent>
 
-  if (data.length) {
-
-    let true_n = data[0].true_negative;
-    if (!data) true_n = '0.0';
-
-    let false_p = data[0].false_positive;
-    if (!data) false_p = '0.0';
-
-    let false_n = data[0].false_negative;
-    if (!data) false_n = '0.0';
-
-    let true_p = data[0].true_positive;
-    if (!data) true_p = '0.0';
-  }
-
-
-  const Plot = (() => {
-
-    if (data.length == 1) {
-      <div className=" col-span-3 p-5">
-        <div className=" flex items-center justify-center  text-center flex-col">
-          <img className="w-4/12" src="/no.png" />
-          <p className="subtitle mt-5 text-gray-600">Sem dados no momento</p>
-        </div>
+  ) : <div className=" col-span-3 p-5">
+      <div className=" flex items-center justify-center  text-center flex-col">
+        <img className="w-4/12" src="/no.png" />
+        <p className="subtitle mt-5 text-gray-600">Sem dados no momento</p>
       </div>
-    }
-
-  })
+    </div>
 
 
-  // const Plot = (data.length != 1) ? (
-  // <HeatMapComponent
-  //   id="heatmap"
-  //   xAxis={{
-  //     valueType: 'Category',
-  //     labels: ['0', '1'],
-  //   }}
-  //   yAxis={{
-  //     valueType: 'Category',
-  //     labels: ['1', '0'],
-  //   }}
-  //   paletteSettings={{
-  //     palette: [
-  //       { color: '#C06C84' },
-  //       { color: '#6C5B7B' },
-  //       { color: '#355C7D' },
-  //     ],
-  //     type: 'Gradient',
-  //   }}
-  //   dataSource={[
-  //     [true_n, false_p],
-  //     [false_n, true_p],
-  //   ]}
-  // >
-  //   <Inject services={[Adaptor, Legend, Tooltip]} />
-  // </HeatMapComponent>
 
-  // ) : <div className=" col-span-3 p-5">
-  //     <div className=" flex items-center justify-center  text-center flex-col">
-  //       <img className="w-4/12" src="/no.png" />
-  //       <p className="subtitle mt-5 text-gray-600">Sem dados no momento</p>
-  //     </div>
-  //   </div>
 
   return <Chart title={menssage} crop={crop}> {Plot} </Chart>
 
-  // return <Chart title={menssage} crop={crop}> {(data.length) != 1 ? data.length : "nãoteste"} </Chart>
+  //  return <Chart title={menssage} crop={crop}> {(data.length) ? data[0].true_positive : "nãoteste"} </Chart>
 
 };
 
