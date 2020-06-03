74efb426a0e730be1a4c248fed14089dd7b493ca
KaioGama <kaioeduardoescar@gmail.com>
2020-06-03 14:49:31 -0400

    [Confusion]
    sem Confusion
    
    Co-Authored-By: Daniel Bispo do Vale <danielbispov@gmail.com>

--- src/components/Plots/Plots.jsx
+++ src/components/Plots/Plots.jsx
@@ -7,7 +7,25 @@ const Plots = ({ crop }) => {
       <Ks crop={crop} />
       <ROC crop={crop} />
       <Mixed crop={crop} />
-      <Confusion crop={crop} />
+      {/* <Confusion crop={crop} /> */}
+
+      {/* <div class="grid grid-rows-3 col-span-3 grid-flow-col gap-4 bg-red-400">
+
+        <div class="grid   col-span-2 grid-flow-col gap-4 bg-red-600">
+          12312
+      </div>
+
+        <div class="grid col-span-3 grid-flow-col gap-4">
+          12312
+      </div>
+       
+        {/* <div class="row-span-1 col-span-2 bg-red-400">12312</div>
+        <div class="row-span-2 col-span-2 bg-red-300">12333</div>
+        <div class="row-span-3 bg-red-500">3213</div>
+        
+        </div> */}
+
+
     </>
   ) : <div className=" col-span-3  flex items-center justify-center p-5">
       <div className="flex items-center justify-center text-center flex-col">
